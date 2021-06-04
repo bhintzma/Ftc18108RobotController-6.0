d4f1a637e7504e8198d1ddebfd54a2b77960b5b3
Brian Hintzman <brian.hintzman@oracle.com>
2021-06-03 17:58:45 -0700

    Updated comment in Anirudh_Driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Anirudh_Driving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Anirudh_Driving.java
@@ -49,8 +49,6 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-//Trial test
-
 
 @TeleOp(name="Mecanum Driving", group="Linear Opmode")
 // @Disabled
